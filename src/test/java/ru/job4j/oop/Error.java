0d8f7f71bb105e2c691eb56970f3a66134352a0d
Aleksei.Zhabura <kkthxkk@gmail.com>
2021-10-15 19:52:46 +0300

    1.7. Конструктор [#173889]

---
+++ src/test/java/ru/job4j/oop/Error.java
@@ -0,0 +1,33 @@
+package ru.job4j.oop;
+
+public class Error {
+
+    boolean active;
+    int status;
+    String message;
+
+    public Error() {
+    }
+
+    public Error(boolean active, int status, String message) {
+        this.active = active;
+        this.status = status;
+        this.message = message;
+
+    }
+
+    public static void main(String[] args) {
+        Error error = new Error(true, 500, "Ошибка ответа от сервера");
+        error.print();
+        Error error1 = new Error();
+        error1.print();
+    }
+
+    public void print () {
+        System.out.println("Ошибка: " + active);
+        System.out.println("Статус ошибки: " + status);
+        System.out.println("Сообщение: " + message);
+    }
+
+
+}
