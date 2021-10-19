5f175505920ec30964280d381fe85632144c3e0b
Aleksei.Zhabura <kkthxkk@gmail.com>
2021-10-19 22:56:21 +0300

    2. Модель данных. [#174683]

---
+++ src/main/java/ru/job4j/tracker/pojo/College.java
@@ -0,0 +1,15 @@
+package ru.job4j.tracker.pojo;
+
+import java.util.Date;
+
+public class College {
+
+    public static void main(String[] args) {
+        Student student = new Student();
+        student.setName("Aleksei Zhabura");
+        student.setGroup("1");
+        student.setAdmission(new Date());
+
+        System.out.println("Студент: " + student.getName() + ". Группа: " + student.getGroup() + ". Дата поступления: " + student.getAdmission());
+    }
+}
