09c193a0d5c9f897b3f65a5d90cbc992a94de46e
Aleksei.Zhabura <kkthxkk@gmail.com>
2021-10-19 13:46:37 +0300

    2. Перегрузка конструктора [#173890]

--- src/main/java/ru/job4j/tracker/Item.java
+++ src/main/java/ru/job4j/tracker/Item.java
@@ -4,6 +4,33 @@ public class Item {
     private int id;
     private String name;
 
+    public Item(){
+    }
+
+    public Item(int id){
+        this.id = id;
+    }
+
+    public Item(int id, String name) {
+        this.id = id;
+        this.name = name;
+    }
+
+    public void print() {
+        System.out.println("id = " + id);
+        System.out.println("name = " + name);
+        System.out.println();
+    }
+
+    public static void main(String[] args) {
+        Item first = new Item();
+        first.print();
+        Item second = new Item(1);
+        second.print();
+        Item thrid = new Item(2, "I'm constructor");
+        thrid.print();
+    }
+
     public int getId() {
         return id;
     }
