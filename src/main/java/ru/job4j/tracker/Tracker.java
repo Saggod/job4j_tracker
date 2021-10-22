6646e0fa4693bf4cfafad176fbc69e7af61f88e9
Aleksei.Zhabura <kkthxkk@gmail.com>
2021-10-22 15:37:43 +0300

    3. Реализация профессий в коде [#6837 #218148]

--- src/main/java/ru/job4j/tracker/Tracker.java
+++ src/main/java/ru/job4j/tracker/Tracker.java
@@ -5,13 +5,13 @@ public class Tracker {
     private int ids = 1;
     private int size = 0;
 
-    public Item add(Item item) { //добавление заявок
+    public Item add(Item item) {
         item.setId(ids++);
         items[size++] = item;
         return item;
     }
 
-    public Item findById(int id) { //получение заявки по id
+    public Item findById(int id) {
         Item rsl = null;
         for (int index = 0; index < size; index++) {
             Item item = items[index];
@@ -22,14 +22,5 @@ public class Tracker {
         }
         return rsl;
     }
-
-//    public Item[] findByName(String key) { //получение списка по имени
-//
-//    }
-//
-//
-//    public Item[] findAll() { //получение списка всех заявок
-//
-//    }
 
 }
