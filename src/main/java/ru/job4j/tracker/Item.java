16e0d7424b5642c87c8fce0cb4165107b07affbc
Aleksei.Zhabura <kkthxkk@gmail.com>
2021-10-23 16:03:26 +0300

    5. Tracker - хранилище [#396]

--- src/main/java/ru/job4j/tracker/Item.java
+++ src/main/java/ru/job4j/tracker/Item.java
@@ -1,17 +1,20 @@
 package ru.job4j.tracker;
 
 import java.time.LocalDateTime;
+import java.util.Objects;
 
 public class Item {
     private int id;
+
     private String name;
+
     private LocalDateTime created = LocalDateTime.now();
 
     public Item() {
     }
 
-    public Item(int id) {
-        this.id = id;
+    public Item(String name) {
+        this.name = name;
     }
 
     public Item(int id, String name) {
@@ -19,24 +22,41 @@ public class Item {
         this.name = name;
     }
 
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) {
+            return true;
+        }
+        if (o == null || getClass() != o.getClass()) {
+            return false;
+        }
+        Item item = (Item) o;
+        return id == item.id
+                && Objects.equals(name, item.name);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, name);
+    }
+
     public int getId() {
         return id;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public LocalDateTime getCreated() {
-        return created;
     }
 
     public void setId(int id) {
         this.id = id;
     }
 
+    public String getName() {
+        return name;
+    }
+
     public void setName(String name) {
         this.name = name;
     }
 
+    public LocalDateTime getCreated() {
+        return created;
+    }
 }
--- src/main/java/ru/job4j/tracker/Tracker.java
+++ src/main/java/ru/job4j/tracker/Tracker.java
@@ -1,5 +1,7 @@
 package ru.job4j.tracker;
 
+import java.util.Arrays;
+
 public class Tracker {
     private final Item[] items = new Item[100];
     private int ids = 1;
@@ -22,4 +24,34 @@ public class Tracker {
         }
         return rsl;
     }
+
+    public Item[] findByName(String key) {
+        Item[] findName = new Item[size];
+        int count = 0;
+        for (int i = 0; i < size; i++) {
+            Item name = items[i];
+            boolean eq = name.getName().equals(key);
+            if (eq) {
+                findName[i] = name;
+                count++;
+            }
+        }
+        findName = Arrays.copyOf(findName, count);
+        return findName;
+    }
+
+    public Item[] findAll() {
+        Item[] nameWithounNull = new Item[size];
+        int count = 0;
+        for (int i = 0; i < size; i++) {
+            Item name = items[i];
+            if (name != null) {
+                nameWithounNull[count] = name;
+                count++;
+            }
+        }
+        nameWithounNull = Arrays.copyOf(nameWithounNull, count);
+        return nameWithounNull;
+    }
 }
+
