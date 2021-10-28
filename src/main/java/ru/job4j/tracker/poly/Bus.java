3de246046f0370a7c64588d24ebedf495f600157
Aleksei.Zhabura <kkthxkk@gmail.com>
2021-10-28 15:46:14 +0300

    3. Интерфейс Input [#181092 #221238]

--- src/main/java/ru/job4j/tracker/poly/Bus.java
+++ src/main/java/ru/job4j/tracker/poly/Bus.java
@@ -1,6 +1,6 @@
 package ru.job4j.tracker.poly;
 
-public class Bus implements Vehicle {
+public class Bus implements Vehicle, Transport {
     @Override
     public void numberVehicle() {
         System.out.print(getClass().getSimpleName() + " A 123 AA");
@@ -9,5 +9,16 @@ public class Bus implements Vehicle {
     @Override
     public void move() {
         System.out.println(" Движется по скоростной трассе");
+    }
+
+    @Override
+    public void count(int passengers) {
+        System.out.println("Максимум пвссажиров = " + passengers);
+
+    }
+
+    @Override
+    public int charge(int fuel) {
+        return fuel * 5;
     }
 }
