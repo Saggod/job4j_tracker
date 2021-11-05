214550af5f89ba9d33fc65f0182861711edf3673
Aleksei.Zhabura <kkthxkk@gmail.com>
2021-11-05 14:34:53 +0300

    0.4. Пользовательские исключения. [#219367 #224516]

--- src/main/java/ru/job4j/tracker/ex/FindEl.java
+++ src/main/java/ru/job4j/tracker/ex/FindEl.java
@@ -3,15 +3,12 @@ package ru.job4j.tracker.ex;
 public class FindEl {
     public static int indexOf(String[] value, String key) throws ElementNotFoundException {
         int rsl = -1;
-        if (key == null) {
-            throw new ElementNotFoundException("key cannot be null");
-        }
-        for (int i = 0; i < value.length; i++) {
-            if (value[i] == key) {
-                System.out.println("index = " + i);
+        for (String i : value) {
+            if (i.equals(key)) {
+                return rsl;
             }
         }
-        return rsl;
+        throw new ElementNotFoundException("Element not exist");
     }
 
     public static boolean sent(String value, String[] abuses) throws ElementAbuseException {
@@ -31,7 +28,7 @@ public class FindEl {
     public static void main(String[] args) {
         String[] value = {"test", "cat", "dog", "bird"};
         try {
-            indexOf(value, "cat");
+            indexOf(value, "dog");
         } catch (ElementNotFoundException e) {
             e.printStackTrace();
         }
