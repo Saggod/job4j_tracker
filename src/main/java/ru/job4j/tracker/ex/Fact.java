9514c5542c95e14005d81202f62f55be596ea29e
Aleksei.Zhabura <kkthxkk@gmail.com>
2021-11-07 17:22:47 +0300

    0.6. Error - исключения, связанные с работой виртуальной машины [#219370 #224822]

--- src/main/java/ru/job4j/tracker/ex/Fact.java
+++ src/main/java/ru/job4j/tracker/ex/Fact.java
@@ -5,13 +5,12 @@ public class Fact {
         public static int calc(int n) {
             if (n == 0 || n == 1) {
                 return 1;
-            } else {
-                return calc(n - 1) * n;
             }
+                return calc(n - 1) * n;
         }
 
         public static void main(String[] args) {
-            int rsl = calc(4);
+            int rsl = calc(1);
             System.out.println(rsl);
         }
 }
