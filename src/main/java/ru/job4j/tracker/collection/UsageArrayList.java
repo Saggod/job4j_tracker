4943a66f1cfe256cf36f9befe592f68c442e636f
Aleksei.Zhabura <kkthxkk@gmail.com>
2021-12-11 13:26:17 +0300

    0. Коллекции, ArrayList, List, Обобщения [#10094]

---
+++ src/main/java/ru/job4j/tracker/collection/UsageArrayList.java
@@ -0,0 +1,18 @@
+package ru.job4j.tracker.collection;
+
+import java.util.ArrayList;
+
+public class UsageArrayList {
+
+    public static void main(String[] args) {
+        ArrayList<String> name = new ArrayList<String>();
+
+        name.add("Petr");
+        name.add("Ivan");
+        name.add("Stepan");
+
+        for (Object value : name) {
+            System.out.println(value);
+        }
+    }
+}
