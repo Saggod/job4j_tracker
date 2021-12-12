96857dde3e2dc7b0725a629cfb0cdf2af321d205
Aleksei.Zhabura <kkthxkk@gmail.com>
2021-12-12 15:03:43 +0300

    0. Коллекции, ArrayList, List, Обобщения [#10094 #238647]

--- src/main/java/ru/job4j/tracker/collection/UsageArrayList.java
+++ src/main/java/ru/job4j/tracker/collection/UsageArrayList.java
@@ -6,12 +6,10 @@ public class UsageArrayList {
 
     public static void main(String[] args) {
         ArrayList<String> name = new ArrayList<String>();
-
         name.add("Petr");
         name.add("Ivan");
         name.add("Stepan");
-
-        for (Object value : name) {
+        for (String value : name) {
             System.out.println(value);
         }
     }
